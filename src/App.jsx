5bcb413241d230c749100cf6d5556e00cc4d73fa
Jeong Hoon <gbg9609@naver.com>
2024-05-16 23:24:05 +0900

    리액트 훅 - Link, useNavigate, 동적 경로 : URL 파라미터 이미지, 폰트 파일 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+import emotion1 from "./assets/emotion1.png";
+import emotion2 from "./assets/emotion2.png";
+import emotion3 from "./assets/emotion3.png";
+import emotion4 from "./assets/emotion4.png";
+import emotion5 from "./assets/emotion5.png";
+
+// 1. "/" : 모든 일기를 조회하는 Home 페이지
+// 2. "/new" : 새로운 일기를 작성하는 New 페이지
+// 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 function App() {
-  const [count, setCount] = useState(0)
+  const nav = useNavigate();
+
+  const onClickButton = () => {
+    nav("/new");
+  };
 
   return (
+    // Routes 컴포넌트 안에는 Route 컴포넌트만 들어갈 수 있음 div 같은 태그요소를 넣을 수 없음
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={emotion1} alt="" />
+        <img src={emotion2} alt="" />
+        <img src={emotion3} alt="" />
+        <img src={emotion4} alt="" />
+        <img src={emotion5} alt="" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/new"}>New</Link>
+        <Link to={"/diary"}>Diary</Link>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/new" element={<New />} />
+        <Route path="/diary/:id" element={<Diary />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
     </>
-  )
+    // path="*" -> * - 와일드 카드, 마치 switch문에 default와 비슷하다고
+    // 생각하면 됨
+  );
 }
 
-export default App
+export default App;
