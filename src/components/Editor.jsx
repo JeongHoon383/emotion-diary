51c8f35273f65bf068d254d77e8b85d66063b61a
Jeong Hoon <gbg9609@naver.com>
2024-05-31 23:40:05 +0900

    new 페이지 기능 구현, 사용자 입력 값 중 오늘의 날짜 state에 저장 및 화면에 랜더링

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import EmotionItem from "./EmotionItem";
 import "./Editor.css";
 import Button from "./Button";
@@ -26,14 +26,55 @@ const emotionList = [
   },
 ];
 
+const getStringedDate = (targetDate) => {
+  // date : YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
 const Editor = () => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+
   const emotionId = 5;
+
+  const onChangeInput = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if ((name = "createdDate")) {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
 
   return (
     <div className="Editor">
       <section className="date_section">
         <h4>오늘의 날짜</h4>
-        <input type="date" />
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
       </section>
       <section className="emotion_section">
         <h4>오늘의 감정</h4>
