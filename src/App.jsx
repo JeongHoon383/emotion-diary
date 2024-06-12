93c67ee57f3e63b0717d4aec784e97ce5aaee980
Jeong Hoon <gbg9609@naver.com>
2024-06-12 21:09:27 +0900

    localStorage로 Diary 데이터 저장 및 배포준비

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,97 @@
 import "./App.css";
-import { useReducer, useRef, createContext } from "react";
+import { useReducer, useRef, createContext, useEffect, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
-import Edit from "./components/Edit";
+import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
 // 1. "/" : 모든 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
-const mockData = [
-  {
-    id: 1,
-    createdDate: new Date("2024-05-21").getTime(),
-    emotionId: 4,
-    content: "1번 일기 내용",
-  },
-  {
-    id: 2,
-    createdDate: new Date("2024-05-20").getTime(),
-    emotionId: 3,
-    content: "2번 일기 내용",
-  },
-  {
-    id: 3,
-    createdDate: new Date("2024-04-06").getTime(),
-    emotionId: 2,
-    content: "3번 일기 내용",
-  },
-  {
-    id: 4,
-    createdDate: new Date().getTime(),
-    emotionId: 1,
-    content: "4번 일기 내용",
-  },
-];
-
 function reducer(state, action) {
+  let nextState;
+  // 새로운 state 값을 저장 할 변수
+
   switch (action.type) {
-    case "CREATE":
-      return [action.data, ...state];
-    case "UPDATE":
-      return state.map((item) =>
+    case "INIT":
+      return action.data;
+    // INIT case는 다른 case 와 다르게 nextState 변수에 담지 않은 이유
+    // nextState는 localStorage에 변경된 데이터의 값을 저장해주는 역할
+    // but, INIT case는 action.data 의 값은 애초에 locaStorage에서 불러온 값이기 때문
+    // 한번 더 불러올 필요 없음
+    case "CREATE": {
+      nextState = [action.data, ...state];
+      break;
+    }
+
+    case "UPDATE": {
+      nextState = state.map((item) =>
         String(item.id) === String(action.data.id) ? action.data : item
       );
-    case "DELETE":
-      return state.filter((item) => item.id !== action.id);
+      break;
+    }
+    case "DELETE": {
+      nextState = state.filter((item) => item.id !== action.id);
+      break;
+    }
     default:
       return state;
   }
+
+  localStorage.setItem("diary", JSON.stringify(nextState));
+  return nextState;
 }
 
 export const DiaryStateContext = createContext();
 export const DiaryDispatchContext = createContext();
 
 function App() {
-  const [data, dispatch] = useReducer(reducer, mockData);
-  const idRef = useRef(5);
+  const [isLoading, setIsLoading] = useState(true);
+  // 만약 useEffect 가 호출되기 전 즉 dispatch 함수가 데이터 state의 일기
+  // 데이터를 저장해두기 전에 다른 페이지 컴포넌트들이 미리 랜더링이 진행된다면?
+  // useDiary가 먼저 진행되어 빈 배열을 반환할 수가 있음.
+  // 해결 방법 : 로딩 기능을 만들어주면 됨.
+  // 로딩 상태를 보관하는 state 생성
+  // dispatch 함수가 실행되어서 데이터 state의 초기값을 설정하는 순간 로딩이 끝나면 됨
+  const [data, dispatch] = useReducer(reducer, []);
+  const idRef = useRef(0);
+
+  useEffect(() => {
+    const storedData = localStorage.getItem("diary");
+    if (!storedData) {
+      setIsLoading(false);
+      return;
+    }
+    // undefined 또는 null 값이 반환됐을때 예외처리
+
+    const parsedData = JSON.parse(storedData);
+    if (!Array.isArray(parsedData)) {
+      // 배열이라면 false 아니라면 true -> ! not 연산 붙음
+      setIsLoading(false);
+      return;
+    }
+    // parsedData의 값이 배열이 아닌 경우 예외처리
+
+    let maxId = 0;
+    parsedData.forEach((item) => {
+      if (Number(item.id) > maxId) {
+        maxId = Number(item.id);
+      }
+    });
+
+    idRef.current = maxId + 1;
+    // 새로운 일기를 추가할 때 id가 겹칠 수 있음
+    // 가장 높은 Id를 찾아서 + 1 시켜줌.
+
+    dispatch({
+      type: "INIT",
+      data: parsedData,
+    });
+    setIsLoading(false);
+  }, []);
 
   // 새로운 일기 추가
   const onCreate = (createdDate, emotionId, content) => {
@@ -94,6 +127,12 @@ function App() {
       id,
     });
   };
+
+  if (isLoading) {
+    return <div>데이터 로딩중입니다 ...</div>;
+  }
+  // 로딩이 끝나지 않았을 때 페이지들도 랜더링 하면 안됨
+  // 로딩중일때 조건문도 추가 해줄것
 
   return (
     // Routes 컴포넌트 안에는 Route  컴포넌트만 들어갈 수 있음 div 같은 태그요소를 넣을 수 없음
